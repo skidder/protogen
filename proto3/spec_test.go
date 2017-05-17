382dcd54f26eaab108a4d514959c8aa7eaed7d81
Scott Kidder <scott@mux.com>
2017-05-17 11:35:55 -0700

    Add example for writing specification

--- proto3/spec_test.go
+++ proto3/spec_test.go
@@ -1,6 +1,9 @@
 package proto3
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestScalarField_Validate(t *testing.T) {
 	type fields struct {
@@ -125,9 +128,114 @@ func TestSpec_Write(t *testing.T) {
 			t.Errorf("%q. Spec.Write() error = %v, wantErr %v", tt.name, err, tt.wantErr)
 			continue
 		}
-		t.Logf("Generated protobuf spec:\n%s\n", got)
-		// if got != tt.want {
-		// 	t.Errorf("%q. Spec.Write() = %v, want %v", tt.name, got, tt.want)
-		// }
+		if tt.wantErr == false && got == "" {
+			t.Error("String representing Protobuf spec was unexpectedly empty")
+			continue
+		}
 	}
+}
+
+func ExampleWrite() {
+	spec := &Spec{
+		Package: "foo",
+		Messages: []Message{
+			Message{
+				Name: "Beacon",
+				Messages: []Message{
+					Message{
+						Name: "Event",
+						ReservedValues: []Reserved{
+							ReservedTagValue{Tag: 1},
+							ReservedTagValue{Tag: 2},
+							ReservedTagValue{Tag: 3},
+							ReservedTagRange{LowerTag: 6, UpperTag: 9},
+						},
+						Fields: []Field{
+							CustomField{Name: "Habitat", Typing: "string", Tag: 10, Rule: REPEATED, Comment: "What am I?"},
+							ScalarField{Name: "Continent", Typing: STRING_TYPE, Tag: 11, Comment: "Where am I?"},
+							MapField{Name: "LanguageMap", KeyTyping: STRING_TYPE, ValueTyping: STRING_TYPE, Tag: 12, Comment: "Super essential"},
+						},
+					},
+				},
+				ReservedValues: []Reserved{
+					ReservedTagValue{Tag: 1},
+					ReservedTagValue{Tag: 2},
+					ReservedTagValue{Tag: 3},
+					ReservedTagRange{LowerTag: 6, UpperTag: 9},
+				},
+				Fields: []Field{
+					CustomField{Name: "Habitat", Typing: "string", Tag: 20, Rule: REQUIRED, Comment: "What am I?"},
+					ScalarField{Name: "Continent", Typing: STRING_TYPE, Tag: 21, Rule: OPTIONAL, Comment: "Where am I?"},
+					MapField{Name: "LanguageMap", KeyTyping: STRING_TYPE, ValueTyping: STRING_TYPE, Tag: 22, Comment: "Super essential"},
+					CustomMapField{Name: "CustomMap", KeyTyping: STRING_TYPE, ValueTyping: "Event", Tag: 23},
+				},
+				Enums: []Enum{
+					Enum{
+						Name: "Country",
+						Values: []EnumValue{
+							EnumValue{Name: "US", Tag: 0},
+							EnumValue{Name: "CA", Tag: 1, Comment: "Canada"},
+							EnumValue{Name: "GB", Tag: 2, Comment: "Great Britain"},
+							EnumValue{Name: "MX", Tag: 3, Comment: "Mexico"},
+						},
+					},
+					Enum{
+						Name:       "PlaybackState",
+						AllowAlias: true,
+						Values: []EnumValue{
+							EnumValue{Name: "Waiting", Tag: 0},
+							EnumValue{Name: "Playing", Tag: 1},
+							EnumValue{Name: "Started", Tag: 1},
+							EnumValue{Name: "Stopped", Tag: 2},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	s, err := spec.Write()
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(s)
+	}
+
+	// Output:
+	// syntax = "proto3";
+	// package foo;
+	//
+	// message Beacon {
+	//   message Event {
+	//     reserved 1;
+	//     reserved 2;
+	//     reserved 3;
+	//     reserved 6 to 9;
+	//     repeated string Habitat = 10;   // What am I?
+	//     string Continent = 11;   // Where am I?
+	//     map<string, string> LanguageMap = 12;   // Super essential
+	//   }
+	//
+	//   enum Country {
+	//     US = 0;
+	//     CA = 1;   // Canada
+	//     GB = 2;   // Great Britain
+	//     MX = 3;   // Mexico
+	//   }
+	//   enum PlaybackState {
+	//     option allow_alias = true;
+	//     Waiting = 0;
+	//     Playing = 1;
+	//     Started = 1;
+	//     Stopped = 2;
+	//   }
+	//   reserved 1;
+	//   reserved 2;
+	//   reserved 3;
+	//   reserved 6 to 9;
+	//   required string Habitat = 20;   // What am I?
+	//   optional string Continent = 21;   // Where am I?
+	//   map<string, string> LanguageMap = 22;   // Super essential
+	//   map<string, Event> CustomMap = 23;
+	// }
 }
